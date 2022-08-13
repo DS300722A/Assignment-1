4c5bbd7cf95fbc025da6a0b8f372cfcd558421d1
SHAHZADI FATIMA <110473907+DS300722A@users.noreply.github.com>
2022-08-13 13:38:33 -0700

    Add files via upload

---
+++ WAP for odd days.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# dont go outside the odd days
+
+day=input("enter the day : ")
+if day=="sunday" or day=="tuesday" or day=="thursday" or day=="saturday":
+    print("Dont go outside it is odd day")
+else:
+    print("you can go outside it is even day")
+
+
+# In[ ]:
+
+
+
+
